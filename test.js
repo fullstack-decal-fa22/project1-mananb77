b3841c803638a611006aba4f458a94a460c51d97
Manan Bhargava <manan.bhargava@berkeley.edu>
2022-09-30 01:04:08 -0700

    finished work section

---
+++ test.js
@@ -0,0 +1,16 @@
+const x = 5;
+let y = 5;
+y += 1;
+console.log(y);
+
+let monthlyRent = 500;
+const yearlyRent = monthlyRent * 12; 
+console.log(yearlyRent)
+
+function max(number1, number2) {
+  if (number1 > number2) {
+    return number1;
+  } else if (number) {
+    return number2;
+  }
+}
